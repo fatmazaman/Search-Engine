e32849459a393d3f16de00d8292a90d465abf0c9
fatmazaman <fatma302@gmail.com>
2014-12-23 01:06:28 -0500

    find program

---
+++ find.py
@@ -0,0 +1,7 @@
+page =('<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href="http://udacity.com">')
+start_link = page.find('<a href=')
+start_quate = page.find('"',start_link)
+end_quate = page.find('"', start_quate+1)
+url = page[start_quate+1 :end_quate]
+print url
