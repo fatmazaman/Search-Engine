554c084104ffd1985cc013e6d406dc12a6a67488
fatmazaman <fatma302@gmail.com>
2014-12-26 23:09:23 -0500

    procedure

--- find.py
+++ find.py
@@ -5,3 +5,12 @@ start_quate = page.find('"',start_link)
 end_quate = page.find('"', start_quate+1)
 url = page[start_quate+1 :end_quate]
 print url
+
+#procedures & Control
+def get_next_target(S):
+	start_link = S.find('<a href=')
+    start_quate = S.find('"',start_link)
+    end_quate = S.find('"', start_quate+1)
+    url = S[start_quate+1 :end_quate]
+    return url , end_quate 
+
