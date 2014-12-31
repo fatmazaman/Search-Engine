8d96194627a24131aa2c2f0ea2aad882e4bf0ac0
fatmazaman <fatma302@gmail.com>
2014-12-30 23:51:58 -0500

    procedure

---
+++ Procedure.py
@@ -0,0 +1,14 @@
+def get_next_target(page):
+	start_link = page.find('<a href=')
+	if start_link == -1:
+		return None
+    start_quate = page.find('"', start_link)
+    end_quate = page.find('"', start_quate+1)
+    url = page[start_quate+1 :end_quate]
+    return url , end_quate 
+
+url, endpos = get_next_target('Good "Idea" Works')
+if url:
+	print "Here!"
+else:
+	print "Not Here!"
--- find.py
+++ find.py
@@ -7,10 +7,7 @@ url = page[start_quate+1 :end_quate]
 print url
 
 #procedures & Control
-def get_next_target(S):
-	start_link = S.find('<a href=')
-    start_quate = S.find('"',start_link)
-    end_quate = S.find('"', start_quate+1)
-    url = S[start_quate+1 :end_quate]
-    return url , end_quate 
+
+
+
 
