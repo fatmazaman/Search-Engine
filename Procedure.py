622d983c208aa904b994a2ef3f360b42cf299a1b
fatmazaman <fatma302@gmail.com>
2015-01-15 02:17:12 -0500

    Again Stuck

--- Procedure.py
+++ Procedure.py
@@ -1,15 +1,15 @@
 def get_next_target(page):
-	start_link = page.find('<a href=')
-	if start_link == -1:
-		return None
-    start_quate = page.find('"', start_link)
-    end_quate = page.find('"', start_quate+1)
-    url = page[start_quate+1 :end_quate]
-    return url , end_quate 
+    start_link = page.find('<a href=')
+    if start_link == -1:
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
 
 def print_all_links(page):
-	while True:
-        url, endpos = get_next_target('Good "Idea" Works')
+	while 1:
+        url, endpos = get_next_target(page)
         if url:
 	        print url
 	        page = page[endpos:]
